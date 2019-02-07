383e57d2d620760502f0bd1ecd72a972438aa705
Andrew <bloyce1990@gmail.com>
2019-02-07 20:09:35 +1000

    Preparing for hosting

--- server.js
+++ server.js
@@ -33,16 +33,5 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-// TESTING SERVER
-
-app.get("/api/hello", (req, res) => {
-  res.send({ express: "Hello From Express" });
-});
-app.post("/api/world", (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`
-  );
-});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
